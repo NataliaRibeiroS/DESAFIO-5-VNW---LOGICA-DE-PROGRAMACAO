d4c2e37d51360de41b41bf88936c342d2c8ec998
NataliaRibeiroS <nethy.ribeiro10@gmail.com>
2022-01-26 15:30:25 -0300

    Codando
    Inclusão do JS, HTML e CSS

---
+++ script.js
@@ -0,0 +1,21 @@
+//ESTRUTURA DE DADOS COMPLEXOS
+
+// Crie um array que receba 5 itens e exiba no console.
+let frutas = ["melancia", "manga", "banana", "laranja", "morango"]
+console.log(frutas)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+frutas.unshift("maracuja")
+
+// Utilize um método para remover o último nome do array.
+frutas.pop()
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+frutas.push("uva", "goiaba")
+
+// Utilize um método para remover o primeiro nome do array.
+frutas.shift("maracuja")
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
